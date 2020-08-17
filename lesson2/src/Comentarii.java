dbc6f6bd08c56f9ab41897653cd72118e3a2b8a6
Svetlana Bodrug <fishdrug@tahoo.com>
2020-08-17 22:04:09 +0300

    adauga lesson2

---
+++ lesson2/src/Comentarii.java
@@ -0,0 +1,18 @@
+public class Comentarii {
+    public static void main (String[] arg){
+        int a1=10;//atribuire
+        String s="ass";
+        float f =5.2F;
+        long a=1234567890123456789L;
+        double b=a;
+      /*  System.out.println(a);
+        System.out.println(b);*/
+        int w=10;
+        int x=w+1;
+        int z=(x-2)/(2+2);
+       /* System.out.println("z="+z);*/
+        double d=123;
+        int ii=(int)d;
+        System.out.println("ii="+ii);
+    }
+}
