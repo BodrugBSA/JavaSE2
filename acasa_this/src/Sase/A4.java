950215c5e48bb39c21ce01045847a7e750009b67
Svetlana Bodrug <fishdrug@tahoo.com>
2020-08-31 18:06:11 +0300

    tema pe acasa this

--- acasa_this/src/Sase/A4.java
+++ acasa_this/src/Sase/A4.java
@@ -3,4 +3,18 @@ package Sase;
 public class A4 {
     int x;
     int y;
+    public A4(A4 ob){
+        x=ob.x;
+        y=ob.y;
+    }
+    public A4 getA(){
+        return this;
+    }
+A4(){
+
+}
+public String afis(){
+    return " metoda afis ";
+}
+
 }
--- acasa_this/src/Sase/RetObj.java
+++ acasa_this/src/Sase/RetObj.java
@@ -1,9 +1,18 @@
 package Sase;
 
 public class RetObj {
-    int i;
-    A4 obj;
-    RetObj (A4 obj) {
-        this.obj=obj;
+    public static void main(String[] args) {
+        A4 a1=new A4();
+        a1.x=2;
+        a1.y=3;
+        A4 a2=new A4(a1);
+        System.out.println(a2.getA());
+        A4 a3 =new A4();
+        a3.x=11;
+        a3.y=22;
+        System.out.println("a3="+a3.x);
+        A4 a4=a3.getA(); //11 new A4()
+        System.out.println("a4 "+a4.x);
+        System.out.println(a4.afis());
     }
 }
